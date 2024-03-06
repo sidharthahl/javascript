40806ed8c92f449de4f26a471a5d1417a1cf3740
Sidharth <90142740+sidharthahl@users.noreply.github.com>
2024-03-06 11:33:12 +0000

    container config done

---
+++ .devcontainer/01_begg/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123413
+let accountEmail="sidharth@google.com"
+var accountPassword=00000
+/*   --comment--
+   instead of var we use let to declare a keyword because var have issue in block scope and functional scope.
+*/
+accountCity="rohatk"
+/*  
+  in js we can we can declare variable without using
+*/
+let accountState;
+// accountId=4343434
+accountEmail="aditya@google.com"
+accountPassword=1111
+accountCity="jhajjar"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
