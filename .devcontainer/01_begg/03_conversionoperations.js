267b377b384fb2c2f5d21419ef2eee3cebe5683e
Sidharth <90142740+sidharthahl@users.noreply.github.com>
2024-03-06 17:55:05 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_begg/03_conversionoperations.js
@@ -0,0 +1,32 @@
+let score = "33abc"
+ // when it is string and so we can operate it on numbers we use a syntax given below and and if we have a string like 33abc so it have a output nan means not a number. 
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//when 100per cent numbers pe opereation karna h to //
+
+let valueInNumber=Number(score)
+console.log( valueInNumber );
+console.log(typeof valueInNumber);
+  // "33" - 33
+  //"33abc" - NaN
+  // true - 1   false - 0
+
+
+  let isLoggedIn = 1
+  let booleanIsLoggedIn = Boolean(isLoggedIn)
+  console.log(booleanIsLoggedIn);
+   // 1 - true
+   // 0 -- false
+   // ""-- false
+   // "sidharth" -- true
+
+  
+// now we can convert number into string also
+    let someNumber = 33
+    let stringNumber = String(someNumber);
+    console.log(typeof stringNumber);
+ 
+      // string
