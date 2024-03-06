1789a721c3c4b4f190ed582ef0c35092c0120147
Sidharth <90142740+sidharthahl@users.noreply.github.com>
2024-03-06 12:41:35 +0000

    datatypes

---
+++ .devcontainer/01_begg/02_datatypes.js
@@ -0,0 +1,27 @@
+let name ="sidharth Ahlawat" //string datatype//
+let age = 18 // number datatype //
+let isloggedIn = true // boolean datatype//
+   
+   // types of data types // 
+
+              // primitive data types //
+
+   // number - 2 to power 53  
+   // string - "" used to represent text
+   // boolean - true/false
+   // bigint - for very big number 
+   // null - standalone value (representation of empty value)
+   // undefined - we declared a variable and even not given a value
+   // symbol - to represent unique values
+          
+
+              // non primitive //
+   // object 
+   // array
+   // function 
+    
+           console.log(typeof "sidharth Ahlawat");
+           console.log(typeof age);
+           console.log(typeof null);
+           console.log(typeof undefined);
+   
