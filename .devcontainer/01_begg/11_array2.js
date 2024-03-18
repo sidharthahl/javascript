93e61d94a0a287df5839affc50a0ce72206e11c4
Sidharth <90142740+sidharthahl@users.noreply.github.com>
2024-03-18 06:05:40 +0000

    array2

--- .devcontainer/01_begg/11_array2.js
+++ .devcontainer/01_begg/11_array2.js
@@ -12,5 +12,20 @@ const dc=["batman","superman","flash"]
 // another alternate that is spread
 
 const all_new_heros =[...marvel_heros,...dc]  // in it it is not an array all elements spread differently 
-console.log(all_new_heros);
+//console.log(all_new_heros);
 
+const another_array=[1,2,3[4,5,6[7,8,9]]];
+const real_another_array =another_array.flat(Infinity)
+//console.log(another_array);
+
+
+// if we download data from net then it can be in any form object,string,node list then there we can use array to ask its types and can change it
+
+console.log(Array.isArray("sidharth")); // false means itis not an array ,, we can convert it as
+//console.log(Array.from("sidharth"));  // it can convert it into array 
+console.log(Array.from({name: "sidharth"})) // [] empty array
+
+let score1 =100
+let score2 =200
+let score3 =300
+console.log(Array.of(score1,score2,score3));
